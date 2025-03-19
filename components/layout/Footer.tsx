e7a4d5d9e6641c90d7c72dc1fcf77fb14c3e1b37
David Liew <daveliew@gmail.com>
2025-03-19 19:48:56 +0800

    introduce DATE() method to have year be dynamic

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <footer className="bg-gray-800 text-white p-4 mt-8">
-      <p>&copy; 2024 David Liew. All rights reserved.</p>
+      <p>&copy; {currentYear} David Liew. All rights reserved.</p>
     </footer>
   );
 };
